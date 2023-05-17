b064a4f07bdfd7599492e3efdcd02b9017536976
arturoeanton <arturoeanton@gmail.com>
2023-05-16 23:29:22 -0300

    commit

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
-mkdir static/;
+rm -rf static/; #borra el directorio static para que no se acumulen los archivos
 cd app;
+rm -rf .parcel-cache; #borra el cahce de parcel para poder buildear siempre 
 npx parcel build index.html --dist-dir ../static --public-url ./static/;
 cd -;
 python main.py;
