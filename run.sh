9ddc24e76137e839c8a0b7fff70c753a5b64e463
arturoeanton <arturoeanton@gmail.com>
2023-05-16 23:24:02 -0300

    commit

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-cd app
-npx parcel build index.html --dist-dir ../static --public-url ./static/
-cd -
-python main.py
+mkdir static/;
+cd app;
+npx parcel build index.html --dist-dir ../static --public-url ./static/;
+cd -;
+python main.py;
